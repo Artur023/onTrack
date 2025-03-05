bc6623ad5eb7fd308536af6e366d062cd9f42fa6
Artur023 <artur.023@mail.ru>
2025-03-05 15:53:40 +0400

    feat: hash

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -2,23 +2,34 @@
 import { ChartBarIcon, ClockIcon, ListBulletIcon } from '@heroicons/vue/24/solid/index.js';
 import NavItem from '@/components/NavItem.vue';
 import { ref } from 'vue';
+import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '@/constants';
 
-const navList = {
-  timeline: ClockIcon,
-  activities: ListBulletIcon,
-  progress: ChartBarIcon,
+const navItems = {
+  [PAGE_TIMELINE]: ClockIcon,
+  [PAGE_ACTIVITIES]: ListBulletIcon,
+  [PAGE_PROGRESS]: ChartBarIcon,
 };
-const currentPage = ref('activities');
-console.log(currentPage)
+
+function normalizePageHash() {
+  const hash = window.location.hash.slice(1);
+
+  if (Object.keys(navItems).includes(hash)) {
+    return hash;
+  }
+  window.location.hash = PAGE_TIMELINE;
+  return PAGE_TIMELINE;
+}
+
+const currentPage = ref(normalizePageHash());
 </script>
 
 <template>
   <nav class="sticky bottom-0 z-10 bg-white text-3xl">
     <ul class="flex items-center justify-around border-t">
       <NavItem
-        v-for="(icon, page) in navList"
+        v-for="(icon, page) in navItems"
         :key="page"
-        :class="{ 'bg-gray-200': page === currentPage }"
+        :class="{ 'pointer-events-none bg-gray-200': page === currentPage }"
         :href="`#${page}`"
         @click="currentPage = page"
       >
